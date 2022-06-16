b45553cf7894cd55ab8a12e302d0eed1a953c387
chengyenkuo <b08902069@ntu.edu.tw>
2022-06-16 12:40:41 +0800

    packet2testcase 2016-03

--- 2016-03/p2/exec.sh
+++ 2016-03/p2/exec.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+if [ $# -ne 0 ]; then
+	cd testcase/
+	zip -r ../testcase.zip *
+	cd ../
+	rm -r -f testcase/
+	exit
+fi
+
 make
 
 rm -r -f testcase/
@@ -8,13 +16,7 @@ mkdir "testcase"
 ./gen
 for ((i=0;i<=10;i++))
 do
-    ./sol < "testcase/$i.in" > "testcase/$i.out"
+    time ./sol < "testcase/$i.in" > "testcase/$i.out"
 done
-
-if [ $# -ne 0 ]; then
-	cd testcase/
-	zip -r ../packet.zip *
-	cd ../
-fi
 
 make clean
